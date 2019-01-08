2a259346dbe2e81d9dc333c962e4f4f8568eb5c1
yinduo <liyinduo@gmail.com>
2019-01-09 02:37:58 +0800

    Updates

---
+++ precache-manifest.3d9fbb7d8d12e65b4a1717ed221ca63c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce415c5a1154b42c9bf7",
+    "url": "/Mirror-Of-Erised/static/css/main.cb2e1030.chunk.css"
+  },
+  {
+    "revision": "ce415c5a1154b42c9bf7",
+    "url": "/Mirror-Of-Erised/static/js/main.ce415c5a.chunk.js"
+  },
+  {
+    "revision": "400edefbc4255cf0f8a2",
+    "url": "/Mirror-Of-Erised/static/css/1.ec3f48fa.chunk.css"
+  },
+  {
+    "revision": "400edefbc4255cf0f8a2",
+    "url": "/Mirror-Of-Erised/static/js/1.400edefb.chunk.js"
+  },
+  {
+    "revision": "f7b248c3ff921f28667c",
+    "url": "/Mirror-Of-Erised/static/js/runtime~main.f7b248c3.js"
+  },
+  {
+    "revision": "b1a9e10bc5986d2d83830944ba9e2921",
+    "url": "/Mirror-Of-Erised/static/media/EduPhoto1.b1a9e10b.jpeg"
+  },
+  {
+    "revision": "18198eaa4b07d3bd0d6c6dba0962c172",
+    "url": "/Mirror-Of-Erised/static/media/EduPhoto2.18198eaa.jpeg"
+  },
+  {
+    "revision": "ac01b11de802aea448048a2ea0fd101f",
+    "url": "/Mirror-Of-Erised/static/media/EduPhoto3.ac01b11d.jpeg"
+  },
+  {
+    "revision": "4aecca229a0435d6214a69a44530a311",
+    "url": "/Mirror-Of-Erised/static/media/EduPhoto4.4aecca22.jpeg"
+  },
+  {
+    "revision": "98311591e6df994e815f1986793f2afd",
+    "url": "/Mirror-Of-Erised/static/media/Cover-Tablet-Portrait.98311591.jpeg"
+  },
+  {
+    "revision": "bfb55f868ff56e5864946324cc6be3f7",
+    "url": "/Mirror-Of-Erised/static/media/Cover-Phone.bfb55f86.jpeg"
+  },
+  {
+    "revision": "bc3f7e2e4a99957c14d7643dcde5c084",
+    "url": "/Mirror-Of-Erised/static/media/Cover-Tablet-Landscape.bc3f7e2e.jpeg"
+  },
+  {
+    "revision": "432c5e4f04b94be6df84459b6737f90c",
+    "url": "/Mirror-Of-Erised/static/media/Cover-Desktop.432c5e4f.jpeg"
+  },
+  {
+    "revision": "340c0c8a8876e17dbca09b3ee142e84c",
+    "url": "/Mirror-Of-Erised/static/media/MyPatronum.340c0c8a.jpeg"
+  },
+  {
+    "revision": "17edfae71c9526b3e0803dc3dfa9552c",
+    "url": "/Mirror-Of-Erised/static/media/InternPhoto1.17edfae7.jpeg"
+  },
+  {
+    "revision": "c9c55a578dce01479f53327f1fda4853",
+    "url": "/Mirror-Of-Erised/static/media/InternPhoto2.c9c55a57.jpeg"
+  },
+  {
+    "revision": "927561be86c4d0fc00c1baad9e708d46",
+    "url": "/Mirror-Of-Erised/static/media/InternPhoto3.927561be.jpeg"
+  },
+  {
+    "revision": "8438259f3629aacdf3e76d0e15507e2e",
+    "url": "/Mirror-Of-Erised/static/media/InternPhoto4.8438259f.jpeg"
+  },
+  {
+    "revision": "12d0d507a96ae9865e7069aa696c3f34",
+    "url": "/Mirror-Of-Erised/static/media/WorkBasketballLarge.12d0d507.mp4"
+  },
+  {
+    "revision": "bdc51a6d20d0b942b59158c78488597b",
+    "url": "/Mirror-Of-Erised/static/media/WorkGroupPhoto.bdc51a6d.jpeg"
+  },
+  {
+    "revision": "fafccba640329ad8d78fe5b7cc4445d1",
+    "url": "/Mirror-Of-Erised/static/media/WorkSelfie1.fafccba6.jpeg"
+  },
+  {
+    "revision": "95c38b943aeb4e270bcf308a6847bd7b",
+    "url": "/Mirror-Of-Erised/static/media/WorkSelfie2.95c38b94.jpeg"
+  },
+  {
+    "revision": "a4565538b11a22787597ab5247d0cfc4",
+    "url": "/Mirror-Of-Erised/static/media/Testimonials_Yinduo.a4565538.pdf"
+  },
+  {
+    "revision": "cdcde31631e902ef441e1b3d590a4b13",
+    "url": "/Mirror-Of-Erised/index.html"
+  }
+];
